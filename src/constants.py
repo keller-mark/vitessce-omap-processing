285936aeb677bad1ff83673d7d101cffab3b453b
mkeller <7525285+keller-mark@users.noreply.github.com>
2023-05-04 17:01:19 -0400

    WIP: copy files from demo-cell-type-annotations

---
+++ src/constants.py
@@ -0,0 +1,12 @@
+# Adapted from https://github.com/keller-mark/vitessce-demo-cell-type-annotations
+from enum import Enum
+
+CL_ROOT_ID = 'CL:0000000'
+
+
+class COLUMNS(Enum):
+    CELL_ID = "cell_id"
+    ANNOTATION = "annotation"
+    PREDICTION_SCORE = "prediction_score"
+    DATASET_ID = "dataset_id"
+    GLOBUS_ID = "globus_id"
